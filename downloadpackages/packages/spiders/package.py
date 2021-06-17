37a77a2de7f7efdefae5adfa5bddc194d0a7f5c7
hihfy <hellohfy@gmail.com>
2021-06-17 11:28:33 +0800

    正规方式兼容新版本

--- downloadpackages/packages/spiders/package.py
+++ downloadpackages/packages/spiders/package.py
@@ -10,7 +10,7 @@ class PackageSpider(scrapy.Spider):
     name = 'package'
     allowed_domains = ['openmptcprouter.com']
     start_urls = [
-        # f'http://packages.openmptcprouter.com/{Version}/aarch64_cortex-a72',
+        f'http://packages.openmptcprouter.com/{Version}/aarch64_cortex-a72',
                   # f'http://packages.openmptcprouter.com/{Version}/aarch64_cortex-a72/packages',
                   # f'http://packages.openmptcprouter.com/{Version}/aarch64_cortex-a72/base',
                   # f'http://packages.openmptcprouter.com/{Version}/aarch64_cortex-a72/routing',
@@ -24,37 +24,24 @@ class PackageSpider(scrapy.Spider):
                   ]
 
     def parse(self, response: Response):
-
-        all_link = response.xpath('//a/@href')
-        links = []
-        for link in all_link:
-            url = link.extract()
-            url = response.url + url
-            links.append(url)
-            log.info(url)
-
-        is_dirs = []
-        all_text = response.xpath('/html/body/pre/text()')
-        for _text in all_text:
-            text: str = _text.extract()
-            text = text.strip()
-            laststr = text.split(' ')[-1]
-            print("-->" + laststr + "<--")
-            is_dirs.append(laststr=='-')
-
-        for i in range(len(all_link)):
+        trs = response.css('tbody tr')
+        for tr in trs:
             item = PackagesItem()
-            url = links[i]
-            if '../' in url:
-                continue
-            is_dir = is_dirs[i]
-            item['url'] = url
-            item['is_dir'] = is_dir
 
-            if is_dir:
+            size = tr.xpath("./td[@class='size']/text()").extract_first()
+            link = tr.xpath("./td[@class='link']/a/@href").extract_first()
+
+            item['is_dir'] = size == '-'
+            if '../' in link:
+                continue
+
+            url = response.url + link
+            item['url'] = url
+            if item['is_dir']:
                 yield scrapy.Request(url, callback=self.parse)
             else:
                 yield item
+
 
 
 
